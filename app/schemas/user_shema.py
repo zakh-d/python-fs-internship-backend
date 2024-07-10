264e91de8436c9ff2c84dabb651081eb2c0b2e78
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-07-10 21:20:15 +0200

    Refactor user schema and related models

--- app/schemas/user_shema.py
+++ app/schemas/user_shema.py
@@ -1,30 +1,39 @@
 from datetime import datetime
+from typing import Annotated, Self
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, model_validator
 
 
 class UserSchema(BaseModel):
     id: UUID
-    username: str
-    email: str
+    username: Annotated[str, Field(min_length=3, max_length=49)]
+    email: Annotated[EmailStr, Field(max_length=49)]
     created_at: datetime
     updated_at: datetime
 
 
 class UserSignInSchema(BaseModel):
     username: str
-    password: str
+    password: Annotated[str, Field(max_length=255)]
 
 
 class UserSignUpSchema(UserSignInSchema):
-    email: str
-    password_confirmation: str
+    email: Annotated[EmailStr, Field(max_length=49)]
+    password_confirmation: str  # can be of any length bc never will be hashed or stored in db
+
+    @model_validator(mode='after')
+    def check_passwords_match(self) -> Self:
+        pw1 = self.password
+        pw2 = self.password_confirmation
+        if pw1 is not None and pw2 is not None and pw1 != pw2:
+            raise ValueError('passwords do not match')
+        return self
 
 
 class UserUpdateSchema(BaseModel):
-    username: str
-    email: str
-    password: str  # password needed to cofirm user intentions
+    username: Annotated[str, Field(min_length=3, max_length=49)]
+    email: Annotated[EmailStr, Field(max_length=49)]
+    password: Annotated[str, Field(max_length=255)]  # password needed to cofirm user intentions
 
 
 class UserDetail(UserSchema):
