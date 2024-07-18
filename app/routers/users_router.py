06db71e608d211d4a42fa25a59857a0fe7903b27
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-07-18 16:23:27 +0200

    added response models to user endpoints

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -9,24 +9,24 @@ from app.services import UserService
 router = APIRouter()
 
 
-@router.get('/')
+@router.get('/', response_model=UserList)
 async def read_users(user_service: Annotated[UserService, Depends(UserService)]) -> UserList:
     return await user_service.get_all_users()
 
 
-@router.post('/sign_up/')
+@router.post('/sign_up/', response_model=UserSchema)
 async def user_sign_up(
     user: UserSignUpSchema, user_service: Annotated[UserService, Depends(UserService)]
 ) -> UserSchema:
     return await user_service.create_user(user)
 
 
-@router.get('/{user_id}')
+@router.get('/{user_id}', response_model=UserDetail)
 async def read_user(user_id: UUID, user_service: Annotated[UserService, Depends(UserService)]) -> UserDetail:
     return await user_service.get_user_by_id(user_id)
 
 
-@router.put('/{user_id}')
+@router.put('/{user_id}', response_model=UserDetail)
 async def update_user(
     user_id: UUID, user: UserUpdateSchema, user_service: Annotated[UserService, Depends(UserService)]
 ) -> UserDetail:
