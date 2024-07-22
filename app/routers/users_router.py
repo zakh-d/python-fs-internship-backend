ad0080fa5388c223b4f2fa13fa6d5d9f50dcde4f
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-07-22 19:56:59 +0200

    refactor: removed auth router bc of partial dublication in users router

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,11 +1,13 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.core.security import get_current_user
-from app.schemas.user_shema import UserDetail, UserList, UserSchema, UserSignUpSchema, UserUpdateSchema
+from app.schemas.user_shema import (
+    UserDetail, UserList, UserSchema, UserSignInSchema, UserSignUpSchema, UserUpdateSchema)
 from app.services import UserService
+from app.services.authentication_service.service import AuthenticationService
 
 router = APIRouter()
 
@@ -18,7 +20,7 @@ async def read_users(
     return await user_service.get_all_users()
 
 
-@router.post('/sign_up/', response_model=UserSchema)
+@router.post('/', response_model=UserSchema)
 async def user_sign_up(
     user: UserSignUpSchema, user_service: Annotated[UserService, Depends(UserService)]
 ) -> UserSchema:
@@ -57,3 +59,14 @@ async def delete_me(
     current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> None:
     await user_service.delete_user(current_user.id)
+
+
+@router.post('/sign_in')
+async def sign_in(
+    user_sign_in: UserSignInSchema, auth_service: Annotated[AuthenticationService, Depends(AuthenticationService)]
+) -> dict[str, str]:
+    user = await auth_service.authenticate(user_sign_in)
+    if user is None:
+        raise HTTPException(status_code=401, detail='Invalid credentials')
+    token = auth_service.generate_jwt_token(user)
+    return {'access_token': token}
