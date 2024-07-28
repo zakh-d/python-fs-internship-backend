9f8cc41f96f12ce009604a2443b3b35b84bf9bb5
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-07-28 16:03:27 +0200

    feat: added invitations and requests models + migration

--- app/db/models.py
+++ app/db/models.py
@@ -1,23 +1,18 @@
+import enum
 from datetime import datetime
 from typing import ClassVar
 from uuid import UUID, uuid4
 
-from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, String, Table, Uuid
+from sqlalchemy import TIMESTAMP, Boolean, Column, Enum, ForeignKey, String, Table, Uuid
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
-
-Base = declarative_base()
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-associate_table = Table(
-    'membership_table',
-    Base.metadata,
-    Column('company_id', ForeignKey('companies.id', ondelete='CASCADE'), primary_key=True),
-    Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), primary_key=True),
-)
+class Base(DeclarativeBase):
+    pass
 
 
-class ModelBase(AsyncAttrs, Base):
+class ModelWithIdAndTimeStamps(AsyncAttrs, Base):
     """Base class for all models containing UUID as primary key."""
 
     __abstract__ = True
@@ -34,7 +29,15 @@ class ModelBase(AsyncAttrs, Base):
     updated_at: Mapped[datetime] = mapped_column(server_default='now()', onupdate=datetime.now)
 
 
-class User(ModelBase):
+associate_table = Table(
+    'membership_table',
+    Base.metadata,
+    Column('company_id', ForeignKey('companies.id', ondelete='CASCADE'), primary_key=True),
+    Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), primary_key=True),
+)
+
+
+class User(ModelWithIdAndTimeStamps):
     __tablename__ = 'users'
 
     username: Mapped[str] = mapped_column(String(50), index=True)
@@ -49,12 +52,14 @@ class User(ModelBase):
     )
 
     participated_companies: Mapped[list['Company']] = relationship(secondary=associate_table, back_populates='members')
+    invitations: Mapped[list['Invitation']] = relationship(back_populates='user')
+    requests: Mapped[list['Invitation']] = relationship(back_populates='user')
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
 
 
-class Company(ModelBase):
+class Company(ModelWithIdAndTimeStamps):
     __tablename__ = 'companies'
 
     name: Mapped[str] = mapped_column(String(50), index=True)
@@ -64,3 +69,37 @@ class Company(ModelBase):
 
     owner: Mapped[User] = relationship(back_populates='companies')
     members: Mapped[list[User]] = relationship(secondary=associate_table, back_populates='participated_companies')
+    invitations: Mapped[list['Invitation']] = relationship(back_populates='company')
+    requests: Mapped[list['Request']] = relationship(back_populates='company')
+
+
+class RequestInviteStatus(enum.Enum):
+    PENDING = 'pending'
+    APPROVED = 'approved'
+    REJECTED = 'rejected'
+
+
+class Invitation(Base):
+    __tablename__ = 'invitations'
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
+    company_id: Mapped[UUID] = mapped_column(ForeignKey('companies.id', ondelete='CASCADE'), primary_key=True)
+    pending: Mapped[RequestInviteStatus] = mapped_column(
+        Enum(RequestInviteStatus, name='invitation_status'), default=RequestInviteStatus.PENDING
+    )
+
+    user: Mapped[User] = relationship(back_populates='invitations')
+    company: Mapped[Company] = relationship(back_populates='invitations')
+
+
+class Request(Base):
+    __tablename__ = 'requests'
+
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
+    company_id: Mapped[UUID] = mapped_column(ForeignKey('companies.id', ondelete='CASCADE'), primary_key=True)
+    pending: Mapped[RequestInviteStatus] = mapped_column(
+        Enum(RequestInviteStatus, name='request_status'), default=RequestInviteStatus.PENDING
+    )
+
+    user: Mapped[User] = relationship(back_populates='requests')
+    company: Mapped[Company] = relationship(back_populates='requests')
