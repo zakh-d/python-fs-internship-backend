3304e276c35777f69540d9171dda894dc782b375
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-08-08 12:28:12 +0200

    refactor: changed only_user_itself protection

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.core.security import get_current_user
 from app.schemas.company_action_schema import CompanyActionSchema
@@ -22,10 +22,9 @@ from app.utils.permissions import only_user_itself
 router = APIRouter()
 
 
-@router.get('/', response_model=UserList)
+@router.get('/', response_model=UserList, dependencies=[Depends(get_current_user)])
 async def read_users(
     user_service: Annotated[UserService, Depends(UserService)],
-    _: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
     page: int = 1,
     limit: int = 10,
 ) -> UserList:
@@ -56,24 +55,15 @@ async def read_user(
     return await user_service.get_user_by_id(user_id)
 
 
-@router.put('/{user_id}', response_model=UserDetail)
-@only_user_itself
+@router.put('/{user_id}', response_model=UserDetail, dependencies=[Depends(only_user_itself)])
 async def update_user(
-    user_id: UUID,
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
-    user: UserUpdateSchema,
-    user_service: Annotated[UserService, Depends(UserService)],
+    user_id: UUID, user: UserUpdateSchema, user_service: Annotated[UserService, Depends(UserService)]
 ) -> UserDetail:
     return await user_service.update_user(user_id, user)
 
 
-@router.delete('/{user_id}')
-@only_user_itself
-async def delete_user(
-    user_id: UUID,
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
-    user_service: Annotated[UserService, Depends(UserService)],
-) -> None:
+@router.delete('/{user_id}', dependencies=[Depends(only_user_itself)])
+async def delete_user(user_id: UUID, user_service: Annotated[UserService, Depends(UserService)]) -> None:
     await user_service.delete_user(user_id)
 
 
@@ -88,80 +78,62 @@ async def sign_in(
     return {'access_token': token}
 
 
-@router.get('{user_id}/invites/')
+@router.get('{user_id}/invites/', dependencies=[Depends(only_user_itself)])
 async def get_invites_for_user(
     user_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> CompanyListSchema:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     return await user_service.get_user_invites(user_id)
 
 
-@router.post('{user_id}/invites/{company_id}')
+@router.post('{user_id}/invites/{company_id}', dependencies=[Depends(only_user_itself)])
 async def accept_invite_to_company(
     user_id: UUID,
     company_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
     company_service: Annotated[CompanyService, Depends(CompanyService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> None:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     await company_service.check_company_exists(company_id)
     await user_service.accept_invitation(user_id, company_id)
 
 
-@router.delete('{user_id}/invites/{company_id}')
+@router.delete('{user_id}/invites/{company_id}', dependencies=[Depends(only_user_itself)])
 async def reject_invite_to_company(
     user_id: UUID,
     company_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
     company_service: Annotated[CompanyService, Depends(CompanyService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> None:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     await company_service.check_company_exists(company_id)
     await user_service.reject_invitation(user_id, company_id)
 
 
-@router.get('{user_id}/requests/')
+@router.get('{user_id}/requests/', dependencies=[Depends(only_user_itself)])
 async def get_user_requests(
     user_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> CompanyListSchema:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     return await user_service.get_user_requests(user_id)
 
 
-@router.post('{user_id}/requests/{company_id}')
+@router.post('{user_id}/requests/{company_id}', dependencies=[Depends(only_user_itself)])
 async def request_to_join_company(
     user_id: UUID,
     company_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
     company_service: Annotated[CompanyService, Depends(CompanyService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> CompanyActionSchema:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     await company_service.check_company_exists(company_id)
     request = await user_service.send_request(user_id, company_id)
     return request
 
 
-@router.delete('{user_id}/requests/{company_id}')
+@router.delete('{user_id}/requests/{company_id}', dependencies=[Depends(only_user_itself)])
 async def cancel_request_to_join_company(
     user_id: UUID,
     company_id: UUID,
     user_service: Annotated[UserService, Depends(UserService)],
     company_service: Annotated[CompanyService, Depends(CompanyService)],
-    current_user: Annotated[UserSchema, Depends(get_current_user)],  # requires authentication
 ) -> None:
-    if current_user.id != user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     await company_service.check_company_exists(company_id)
     await user_service.cancel_request(user_id, company_id)
