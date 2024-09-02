f328da10228da0715781c0ed452ce7ba6974bdfa
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-09-02 21:22:58 +0200

    feat: added separate dockerfile for deploy purposes and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,28 @@
-echo "Deployed"
+if ! command -v docker &> /dev/null; then
+    sudo span install -y docker
+fi
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt install -y nginx
+fi
+
+if [ ! -f /etc/nginx/sites-available/fastapi ]; then
+
+    sudo rm -f /etc/nginx/sites-enabled/default
+    sudo bash -c 'cat > /etc/nginx/sites-available/fastapi <<EOF
+server {
+    listen 80;
+    server_name _;
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+    }
+}
+EOF'
+    sudo ln -s /etc/nginx/sites-available/fastapi /etc/nginx/sites-enabled
+    sudo systemctl restart nginx
+fi
+
+cd service
+docker stop app-container
+docker build -f Dockerfile.prod -t app-image .
+docker run --env-file env -d --name app-container -p 8000:8000 app-image
